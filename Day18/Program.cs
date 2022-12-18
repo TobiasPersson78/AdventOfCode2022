e0cddb53a8f99ab2ab89058c96709d01129c0db6
Tobias Persson <tobias@tobiaspersson.se>
2022-12-18 17:28:00 +0100

    Minor code refactoring

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections.Immutable;
 
-bool useExampleInput = true;
+bool useExampleInput = false;
 
 string inputFilename = useExampleInput
 	? "exampleInput.txt"
 	: "input.txt";
 
-IImmutableSet<(int X, int Y, int Z)> allDroplets =
+IImmutableSet<Point3D> allDroplets =
 	File
 		.ReadAllLines(inputFilename)
 		.Where(item => !string.IsNullOrEmpty(item))
 		.Select(item => item.Split(','))
-		.Select(item => (int.Parse(item[0]), int.Parse(item[1]), int.Parse(item[2])))
+		.Select(item => new Point3D(int.Parse(item[0]), int.Parse(item[1]), int.Parse(item[2])))
 		.ToImmutableHashSet();
 
 int surfaceAreaOfAllDroplets =
@@ -26,32 +26,29 @@ int maxY = allDroplets.Max(item => item.Y) + 1;
 int minZ = allDroplets.Min(item => item.Z) - 1;
 int maxZ = allDroplets.Max(item => item.Z) + 1;
 
-(int X, int Y, int Z) startPosition = (minX, minY, minZ);
-ISet<(int X, int Y, int Z)> visitedPositions = new HashSet<(int X, int Y, int Z)>();
-ISet<(int X, int Y, int Z)> positionsToVisit = new HashSet<(int X, int Y, int Z)>() { startPosition };
+Point3D startPosition = new(minX, minY, minZ);
+ISet<Point3D> visitedPositions = new HashSet<Point3D>();
+ISet<Point3D> positionsToVisit = new HashSet<Point3D>() { startPosition };
 int externalSurfaceAreaOfAllDroplets = 0;
 
 while (positionsToVisit.Any())
 {
-	(int X, int Y, int Z) currentPosition = positionsToVisit.First();
+	Point3D currentPosition = positionsToVisit.First();
 	positionsToVisit.Remove(currentPosition);
 	visitedPositions.Add(currentPosition);
 
-	IEnumerable<(int X, int Y, int Z)> allNeighbors =
-		GetNeighbors(currentPosition).Where(IsValidPosition).ToList();
-	IEnumerable<(int X, int Y, int Z)> alreadyVisitedNeighbors =
-		allNeighbors.Where(neighbor => visitedPositions.Contains(neighbor));
-	IEnumerable<(int X, int Y, int Z)> dropletNeighbors =
-		allNeighbors.Where(neighbor => allDroplets.Contains(neighbor)).ToList();
+	IEnumerable<Point3D> allNeighbors = GetNeighbors(currentPosition).Where(IsValidPosition).ToList();
+	IEnumerable<Point3D> alreadyVisitedNeighbors = allNeighbors.Where(neighbor => visitedPositions.Contains(neighbor));
+	IEnumerable<Point3D> dropletNeighbors = allNeighbors.Where(neighbor => allDroplets.Contains(neighbor)).ToList();
 
 	externalSurfaceAreaOfAllDroplets += dropletNeighbors.Count();
 
-	IEnumerable<(int X, int Y, int Z)> neighborsToVisit =
+	IEnumerable<Point3D> neighborsToVisit =
 		allNeighbors
 			.Except(alreadyVisitedNeighbors)
 			.Except(dropletNeighbors);
 
-	foreach ((int X, int Y, int Z) neighbor in neighborsToVisit)
+	foreach (Point3D neighbor in neighborsToVisit)
 		positionsToVisit.Add(neighbor);
 }
 
@@ -61,17 +58,19 @@ Console.WriteLine($"Surface area of all scanned droplets: {surfaceAreaOfAllDropl
 Console.WriteLine("Day 18B");
 Console.WriteLine($"External surface area of all scanned droplets: {externalSurfaceAreaOfAllDroplets}");
 
-bool IsValidPosition((int X, int Y, int Z) position) =>
+bool IsValidPosition(Point3D position) =>
 	position.X >= minX && position.X <= maxX &&
 	position.Y >= minY && position.Y <= maxY &&
 	position.Z >= minZ && position.Z <= maxZ;
 
-IEnumerable<(int X, int Y, int Z)> GetNeighbors((int X, int Y, int Z) position)
+IEnumerable<Point3D> GetNeighbors(Point3D position)
 {
-	yield return (position.X - 1, position.Y, position.Z); // Left
-	yield return (position.X + 1, position.Y, position.Z); // Right
-	yield return (position.X, position.Y - 1, position.Z); // Down
-	yield return (position.X, position.Y + 1, position.Z); // Up
-	yield return (position.X, position.Y, position.Z - 1); // Towards
-	yield return (position.X, position.Y, position.Z + 1); // Away
+	yield return position with { X = position.X - 1 }; // Left
+	yield return position with { X = position.X + 1 }; // Right
+	yield return position with { Y = position.Y - 1 }; // Down
+	yield return position with { Y = position.Y + 1 }; // Up
+	yield return position with { Z = position.Z - 1 }; // Towards
+	yield return position with { Z = position.Z + 1 }; // Away
 }
+
+record class Point3D(int X, int Y, int Z);
